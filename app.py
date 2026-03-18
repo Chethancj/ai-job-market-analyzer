eade1977e3bef05de5a95ed47d141832d237b9d9
Chethan jayashankar <chethancj111@gmail.com>
2026-03-18 19:06:15 +0000

    Fix import statement for numpy in app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
-import numpy np
+import numpy as np
 
 
 # ======================================
